a7d04d8d3a528836c92efaac298c90f3381da506
Andrii Popov <Popov.Andrey80@gmail.com>
2024-05-13 19:14:28 +0300

    del code 03-mongodb branch

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import path from "path"
-import mongoose from "mongoose";
 import contactsRouter from "./routes/contactsRouter.js";
 import { error } from "console";
 
@@ -23,17 +22,8 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-const DB_HOST = "mongodb+srv://Andrii:TQApGJqwlOMnhY9L@cluster0.twvduok.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0"
+app.listen(3000, () => {
+  console.log("Server is running. Use our API on port: 3000");
+})
 
-mongoose.connect(DB_HOST)
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("Database connection successful");
-      console.log("Server is running. Use our API on port: 3000");
-    })
-  })
-  .catch(error => {
-    console.log(error.message);
-    process.exit(1);
-  })
 
